2f45aaacd665813ef51b84a562c9b7c6126c7251
Mayank <mayankmchandratre@gmail.com>
2024-12-20 20:53:44 +0530

    respo

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,6 +20,7 @@ const config = {
     "@typescript-eslint/no-unused-vars": "off",
     "@typescript-eslint/require-await": "off",
     "@typescript-eslint/prefer-optional-chain":"warn",
+    "@typescript-eslint/no-unsafe-call":"warn",
     "@typescript-eslint/no-misused-promises": [
       "error",
       {
